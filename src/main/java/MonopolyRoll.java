8a5b98d4d2276ff946748614699762e8c61b60db
deepak9962 <dk0137771@gmail.com>
2021-04-27 09:58:59 +0530

    Initial Commit

---
+++ src/main/java/MonopolyRoll.java
@@ -0,0 +1,108 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import javax.security.auth.login.LoginException;
+import java.util.Objects;
+
+public class MonopolyRoll extends ListenerAdapter {
+    public static GuildMessageReceivedEvent eventGlobal;
+
+    public static void main(String[] args) throws LoginException {
+
+        JDA jda = JDABuilder.createDefault("ODM2MDk1NzczNzM3MDkxMDc0.YIZA3g.zLGHKvO7FwB0-BKfINtO5eK7gHo").build();
+        jda.addEventListener(new MonopolyRoll());
+
+        try {
+            while (true) {
+                Thread.sleep(60 * 1000);
+                eventGlobal.getChannel().sendMessage("invoke 1 minute").queue();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        super.onGuildMessageReceived(event);
+        eventGlobal = event;
+        String getMessage = event.getMessage().getContentRaw();
+        if (getMessage.equalsIgnoreCase("!roll")) {
+
+            event.getChannel().sendMessage("Roller Name : " + Objects.requireNonNull(event.getMember()).getUser().getName()).queue();
+            event.getChannel().sendMessage("Total : " + monopolyRoll(6)).queue();
+
+        }
+
+        if (getMessage.equalsIgnoreCase("HelloBot")) {
+            event.getChannel().sendMessage("Fuck me! I'm not in the mood right now. -_-").queue();
+        }
+
+        if (getMessage.equalsIgnoreCase("hibot")) {
+            event.getChannel().sendMessage("Get Lost I said already. Stop bulling me. '-_-'").queue();
+        }
+
+        if (getMessage.equalsIgnoreCase("who are you bot?")) {
+            event.getChannel().sendMessage("Imma MF, leave me alone.").queue();
+        }
+    }
+
+    public int monopolyRoll(int sides) {
+
+        int diceRoll1 = diceRoll(sides);
+        int diceRoll2 = diceRoll(sides);
+        int total;
+        int countRoll = 1;
+
+        eventGlobal.getChannel().sendMessage("Dice 1 : " + diceRoll1 + "\n" + "Dice 2 : " + diceRoll2).queue();
+
+        if (diceRoll1 == diceRoll2) {
+
+            eventGlobal.getChannel().sendMessage("Wow It's a Rolling Double!" + "\n" + "Rolled again!").queue();
+
+            while (diceRoll1 == diceRoll2) {
+
+                int diceRoll3 = diceRoll(sides);
+                int diceRoll4 = diceRoll(sides);
+
+                diceRoll1 = diceRoll1 + diceRoll3;
+                diceRoll2 = diceRoll2 + diceRoll4;
+                countRoll += 1;
+
+                eventGlobal.getChannel().sendMessage("Dice 1 : " + diceRoll3 + "\n" + "Dice 2 : " + diceRoll4).queue();
+
+                if (countRoll == 2) {
+                    if (diceRoll3 == diceRoll4) {
+                        eventGlobal.getChannel().sendMessage("Wow! You got some luck!" + "\n" + "Rolled again!").queue();
+                    }
+                }
+
+                if (countRoll > 2) {
+
+                    if (diceRoll1 == diceRoll2) {
+
+                        eventGlobal.getChannel().sendMessage("You choose to go to hell!").queue();
+                        eventGlobal.getChannel().sendMessage("Roll Counts : " + countRoll).queue();
+                        return -1;
+                    }
+                }
+            }
+        }
+        total = diceRoll1 + diceRoll2;
+
+        eventGlobal.getChannel().sendMessage("Roll Counts : " + countRoll).queue();
+        return total;
+    }
+
+    public int diceRoll(int sides) {
+
+        double diceRoll = Math.random();
+        diceRoll = diceRoll * sides;
+        diceRoll = diceRoll + 1;
+
+        return (int) diceRoll;
+    }
+}
