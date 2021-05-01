47aaa67edbbf85796428ea6fca2a3bc6058f98cd
deepak9962 <56271448+deepak9962@users.noreply.github.com>
2021-05-01 18:42:46 +0530

    Update MonopolyRollAdapter.java

--- src/main/java/MonopolyRollAdapter.java
+++ src/main/java/MonopolyRollAdapter.java
@@ -8,6 +8,7 @@ public class MonopolyRollAdapter {
                 firstDoubleRoll,
                 secondRoll,
                 secondDoubleRoll,
+                thirdRoll,
                 jail,
                 countRoll,
                 totalRolled;
@@ -17,12 +18,13 @@ public class MonopolyRollAdapter {
         firstDoubleRoll = getMessages.getFirstDoubleRoll();
         secondRoll = getMessages.getSecondRoll();
         secondDoubleRoll = getMessages.getSecondDoubleRoll();
+        thirdRoll = getMessages.getSecondRoll();
         jail = getMessages.getJail();
         countRoll = getMessages.getCountRoll();
         totalRolled = getMessages.getTotalRolled();
 
         String messages = "```" + "\n" + userName + "\n" + firstRoll + "\n" + firstDoubleRoll + "\n" + secondRoll + "\n" +
-                secondDoubleRoll + "\n" + jail + "\n" + countRoll + "\n" + totalRolled + "\n" + "```";
+                secondDoubleRoll + "\n" + thirdRoll + "\n" + jail + "\n" + countRoll + "\n" + totalRolled + "\n" + "```";
 
         getMessages.getEventGlobal().getChannel().sendMessage(messages).queue();
     }
